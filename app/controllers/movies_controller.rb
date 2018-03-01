30d16dda301fab9ecc59e0e5f00f057fccc979d2
EC2 Default User <ec2-user@ip-172-31-3-73.us-east-2.compute.internal>
2018-03-01 03:34:04 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,27 +11,43 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all.order(params[:sort])
-    
-    
     #https://stackoverflow.com/questions/9658881/rails-select-unique-values-from-a-column
     @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
     
-    #initially all checked
-    @selected = @all_ratings
-    #pick ones only checked
-    if params[:ratings]
-      @selected = params[:ratings].keys
+    #@movies = @all_ratings
+    
+    if session[:selected_sort].nil?
+      session[:selected_sort] = ""
     end
     
+    if !params[:sort].nil?
+      session[:selected_sort] = params[:sort] #remember the setting
+    end
+    
+    print "\n\n\n" + 'index=>' + session[:selected_ratings].to_s + "\n"
+    if !params[:ratings].nil? #stores session and set @movies
+      print "\n in 1 \n"
+      @selected = params[:ratings].keys
+      @movies = Movie.where(rating: @selected).order(session[:selected_sort])
+      session[:selected_ratings] = params[:ratings]
+    elsif !session[:selected_ratings].nil? #if no ratings selected, then get ratings from session
+      print "\n in 2 \n"
+      @selected = session[:selected_ratings].keys
+      @movies = Movie.where(rating: @selected).order(session[:selected_sort]) 
+    else #no ratings and no sessions, just display all with sort in store
+      print "\n in 3 \n"
+      @selected = @all_ratings
+      @movies = Movie.all.order(session[:selected_sort])
+    end
+
     @selected.each do |rating|
       params[rating] = 1
     end
     
-    if params[:ratings]
-     @movies = Movie.where(rating: @selected)
+    if (params[:sort] == nil or params[:ratings] == nil) and !session[:selected_sort].nil? and !session[:selected_ratings].nil?
+      flash.keep #persist all flash values
+      redirect_to movies_path(:sort => session[:selected_sort], :ratings => session[:selected_ratings])
     end
-   
   end
 
   def new
